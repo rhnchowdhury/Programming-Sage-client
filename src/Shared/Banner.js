f658d51161f431bd5a8a62e1ca797413ddd31b5a
Raihan <rhnchdri@gmail.com>
2022-11-08 20:04:15 +0600

    slider added

---
+++ src/Shared/Banner.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import slider1 from '../Slider/slider1.webp';
+import slider2 from '../Slider/slider2.webp';
+import slider3 from '../Slider/slider3.jpg';
+import './Banner.css'
+const Banner = () => {
+    return (
+
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full design" >
+                <img src={slider1} alt='' className="w-full overlay" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2">
+                    <h1 className='text-6xl font-bold text-white text'>
+                        Skill your self for <span className='ml-14'>your success</span>
+                    </h1>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-3">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full design">
+                <img src={slider2} alt='' className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle mr-3">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full design">
+                <img src={slider3} alt='' className="w-full overlay" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2">
+                    <h1 className='text-6xl font-bold text-white text'>
+                        Skill your self for <span className='ml-14'>your success</span>
+                    </h1>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-3">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Banner;
--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100">
@@ -10,7 +16,8 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+
+                            {menuItems} {/* <li><a>Item 1</a></li>
                             <li tabIndex={0}>
                                 <a className="justify-between">
                                     Parent
@@ -21,14 +28,15 @@ const Header = () => {
                                     <li><a>Submenu 2</a></li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Item 3</a></li> */}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">Programming Sage</a>
+                    <Link to='/' className="btn btn-ghost normal-case text-xl">Programming Sage</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>Item 1</a></li>
+                        {menuItems}
+                        {/* <li><a>Item 1</a></li>
                         <li tabIndex={0}>
                             <a>
                                 Parent
@@ -39,13 +47,14 @@ const Header = () => {
                                 <li><a>Submenu 2</a></li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><a>Item 3</a></li> */}
                     </ul>
                 </div>
                 <div className="navbar-end">
                     <a className="btn">Get started</a>
                 </div>
             </div>
+
         </div>
     );
 };
